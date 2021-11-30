5efcc95851a0ea58081948be41d8ef222506e18d
furkanKotic <furkanpasalioglu@kotic.com.tr>
2021-11-30 22:39:22 +0300

    .

--- app/src/main/java/com/furkanpasalioglu/coinyeni/ui/ayarlar/view/AyarlarActivity.kt
+++ app/src/main/java/com/furkanpasalioglu/coinyeni/ui/ayarlar/view/AyarlarActivity.kt
@@ -1,12 +1,12 @@
 package com.furkanpasalioglu.coinyeni.ui.ayarlar.view
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import com.furkanpasalioglu.coinyeni.data.model.Ayar
 import com.furkanpasalioglu.coinyeni.databinding.ActivityAyarlarBinding
+import com.furkanpasalioglu.coinyeni.ui.main.view.MainActivity.Companion.ayar
 import com.furkanpasalioglu.coinyeni.ui.main.view.MainActivity.Companion.database
 
 class AyarlarActivity : AppCompatActivity() {
@@ -19,21 +19,16 @@ class AyarlarActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
-        val preferences = getSharedPreferences("prefs", Context.MODE_PRIVATE)
-        val edit = preferences.edit()
-        val btc = preferences.getFloat("btc",0F)
-        val tl = preferences.getFloat("tl",0F)
+        val btc = ayar.btc
+        val tl = ayar.tl
 
         binding.btcmiktarEdit.setText(btc.toString())
         binding.tlmiktarText.setText(tl.toString())
 
         binding.button.setOnClickListener {
-            val ayar = Ayar(binding.btcmiktarEdit.text.toString().toFloat(), binding.tlmiktarText.text.toString().toFloat())
-            database.child("ayarlar").setValue(ayar)
-            edit.putFloat("btc",binding.btcmiktarEdit.text.toString().toFloat())
-            edit.putFloat("tl",binding.tlmiktarText.text.toString().toFloat())
-            edit.apply()
-            Toast.makeText(applicationContext,"Kayıt edildi",Toast.LENGTH_SHORT).show()
+            val ayarlar = Ayar(binding.btcmiktarEdit.text.toString().toFloat(), binding.tlmiktarText.text.toString().toFloat())
+            database.child("ayarlar").setValue(ayarlar)
+            Toast.makeText(applicationContext,"Kayıt edildi",Toast.LENGTH_LONG).show()
         }
     }
 
--- app/src/main/java/com/furkanpasalioglu/coinyeni/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/furkanpasalioglu/coinyeni/ui/main/view/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.furkanpasalioglu.coinyeni.ui.main.view
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,7 +11,6 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.furkanpasalioglu.coinyeni.R
@@ -41,7 +39,6 @@ class MainActivity : AppCompatActivity() {
     private val list:ArrayList<DatabaseCoin> = arrayListOf()
     private var databaseCoins : ArrayList<DatabaseCoin> = arrayListOf(DatabaseCoin())
     private var databaseCoinsKeys  = arrayListOf<String>()
-    private lateinit var ayar : Ayar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -192,5 +189,6 @@ class MainActivity : AppCompatActivity() {
     companion object{
         lateinit var anlikBTC : String
         lateinit var database: DatabaseReference
+        lateinit var ayar : Ayar
     }
 }
